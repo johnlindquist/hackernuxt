1a1cf136aefb1a19c3d10c1ef6382f0f8e2d7cd4
John Lindquist <johnlindquist@computer.local>
2017-08-10 14:44:10 -0600

    load-by-route

--- store/index.js
+++ store/index.js
@@ -1,15 +1,10 @@
 import axios from "~/plugins/axios"
 
 export const state = () => ({
-  ids: [],
   items: []
 })
 
 export const mutations = {
-  setIds(state, ids) {
-    state.ids = ids
-  },
-
   setItems(state, items) {
     state.items = items
   }
@@ -25,7 +20,6 @@ export const actions = {
     const itemsResponses = await Promise.all(itemsPromises)
     const items = itemsResponses.map(res => res.data)
 
-    // commit("setIds", ids)
     commit("setItems", items)
   }
 }
