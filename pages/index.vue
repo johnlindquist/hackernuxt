020a1812f89c7f180a15a00c6d90e3a07b5c4499
John Lindquist <johnlindquist@computer.local>
2017-08-10 14:44:57 -0600

    load-by-route

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="code">
-    <ul class="list pa2">
-      <li v-for="item in items" :key="item.id">
-        {{item.title}}
-      </li>
-    </ul>
-  </div>
+    <div class="code">
+        <ul class="list pa2">
+            <li v-for="item in items" :key="item.id">
+                {{item.title}}
+            </li>
+        </ul>
+    </div>
 </template>
 <script>
 import { mapState } from "vuex"
 
 export default {
-  computed: mapState([
-    "ids",
-    "items"
-  ])
+    computed: mapState([
+        "items"
+    ])
 }
 </script>
