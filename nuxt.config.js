4ff1152185343afdb5884f3d02fb7029d88fcece
John Lindquist <johnlindquist@computer.local>
2017-08-10 21:34:12 -0600

    page-transitions

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,8 @@ module.exports = {
   */
   css: [
     "tachyons/css/tachyons.min.css",
-    "tachyons-debug/css/tachyons-debug.min.css"
+    "tachyons-debug/css/tachyons-debug.min.css",
+    "~/assets/main.css"
   ],
   build: {
     vendor: ["axios"]
